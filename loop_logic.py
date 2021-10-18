7db0f0cca14d8a3b5d81f14c95389554278980b6
X _ x <77869381+ThantZawLin@users.noreply.github.com>
2021-10-18 01:57:57 -0400

    Update loop_logic.py

--- loop_logic.py
+++ loop_logic.py
@@ -5,5 +5,12 @@ for i in range(0,n):
         print(" ",end="")
     for x in range(1, (i * 2)):
         print("*",end="")
+        
+for x in range(0,n):
+    print()
+    for y in range(1,x+1):
+        print(" ",end="")
+    for y in range(1,(n-x)*2):
+        print("*",end="")
     
     
