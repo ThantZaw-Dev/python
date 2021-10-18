55b7c6bc406dbfd4f04b2ead28c674ce10255f72
ThantZawLin <bk5509404@gmail.com>
2021-10-18 02:54:42 -0400

    add_while_loop

--- loop_logic.py
+++ loop_logic.py
@@ -6,12 +6,6 @@ for i in range(0,n):
     for x in range(1, (i * 2)):
         print("*",end="")
 
-for x in range(0,n):
-    print()
-    for y in range(1,x+1):
-        print(" ",end="")
-    for y in range(1,(n-x)*2):
-        print("*",end="")
 
     
     
